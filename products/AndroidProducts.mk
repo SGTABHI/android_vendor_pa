faaf98c7ed6f7702acacdcf7b4adc28a8f8942d2
SGTABHI <codingzonehs@gmail.com>
2013-04-09 22:30:43 +0530

    Update AndroidProducts.mk
    Updated for our Device

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -43,3 +43,6 @@ endif
 ifeq (pa_endeavoru,$(TARGET_PRODUCT))
     PRODUCT_MAKEFILES += $(LOCAL_DIR)/pa_endeavoru.mk
 endif
+ifeq (pa_p1,$(TARGET_PRODUCT))
+    PRODUCT_MAKEFILES += $(LOCAL_DIR)/pa_p1.mk
+endif
